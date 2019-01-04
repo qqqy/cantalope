baaf46a0711062193a78b6fff178d3c76c5a0ad7
qqqy <frogasaurus@hotmail.com>
2019-01-04 10:40:34 -0700

    added sessions

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const massive = require('massive')
 require('dotenv').config()
+const session = require('express-session')
 
 const app = express()
 
 const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env
 
 app.use(express.json())
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(function (req, res, next){
+  if(!req.session.thingy){req.session.thingy = {thingy: 'Now he got a damn thingy, OK?'}
+  console.log('Giving dis boi a thingy.')
+} else {console.log('Dis boi gots a thingy!')}
+next()})
 
 // TEST ENDPOINTS //
 app.get('/api/test' , (req, res) => res.sendStatus(200))
